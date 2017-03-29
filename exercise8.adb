c4037b3044bc4f83ad3dc04bc6a35085997e09f2
Space <avn@471.no>
2017-03-29 13:33:24 +0200

    Test implementation of ex8

--- exercise8.adb
+++ exercise8.adb
@@ -8,34 +8,29 @@ procedure exercise8 is
 
     protected type Transaction_Manager (N : Positive) is
         entry Finished;
+	entry Wait_Until_Aborted;
         function Commit return Boolean;
         procedure Signal_Abort;
     private
         Finished_Gate_Open  : Boolean := False;
         Aborted             : Boolean := False;
-        Should_Commit       : Boolean := True;
     end Transaction_Manager;
     protected body Transaction_Manager is
         entry Finished when Finished_Gate_Open or Finished'Count = N is
         begin
-            ------------------------------------------
-            -- PART 3: Complete the exit protocol here
-            ------------------------------------------
-			if Finished'Count = N then
-				Finished_Gate_Open := True;
-				Should_Commit := True;
-			else
-				Finished_Gate_Open := False;
-				if Finished'Count = 0 then
-					Aborted := False;
-				end if;
-			end if;
-			
-			if Aborted = True then
-				Should_Commit := False;
-			end if;
-			
+      		Finished_Gate_Open := true;
+      	
+      		if Finished'Count = 0 then
+      			Finished_Gate_Open := False;
+			Aborted := False;
         end Finished;
+	
+        entry Wait_Until_Aborted when Aborted is
+        begin
+		if Wait_Until_Aborted'Count = 0 then
+			Put_Line("All workers are aborted");
+		end if;
+        end Wait_Until_Aborted;
 
         procedure Signal_Abort is
         begin
@@ -48,8 +43,6 @@ procedure exercise8 is
         end Commit;
         
     end Transaction_Manager;
-
-	
     
     function Unreliable_Slow_Add (x : Integer) return Integer is
     Error_Rate : Constant := 0.15;  -- (between 0 and 1)
@@ -68,9 +61,6 @@ procedure exercise8 is
 		return Result;
     end Unreliable_Slow_Add;
 
-
-
-
     task type Transaction_Worker (Initial : Integer; Manager : access Transaction_Manager);
     task body Transaction_Worker is
         Num         : Integer   := Initial;
@@ -81,33 +71,27 @@ procedure exercise8 is
         Put_Line ("Worker" & Integer'Image(Initial) & " started");
 
         loop
-            Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
-            Round_Num := Round_Num + 1;
 
             ---------------------------------------
             -- PART 2: Do the transaction work here             
             ---------------------------------------
-			
-			begin
-				Num := Unreliable_slow_add(Num);
-			exception
-				when Count_Failed =>
-					Manager.Signal_Abort;
-			end;
-			
-			Manager.Finished;
-            
-            if Manager.Commit = True then
-                Put_Line ("  Worker" & Integer'Image(Initial) & " comitting" & Integer'Image(Num));
-            else
-                Put_Line ("  Worker" & Integer'Image(Initial) &
-                             " reverting from" & Integer'Image(Num) &
-                             " to" & Integer'Image(Prev));
-                -------------------------------------------
-                -- PART 2: Roll back to previous value here
-                -------------------------------------------
-				Num := Prev;
-            end if;
+	    
+	 select 
+	    Manager.Wait_Until_Aborted;
+	    Num := Prev + 5;
+	 then abort
+            Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
+            Round_Num := Round_Num + 1;
+	    begin
+	       Num := Unreliable_Slow_Add(Num);
+	    exception
+	       when Count_Failed =>
+		  Put_Line("Worker " & Integer'Image(Initial) & " aborted");
+		  Manager.Signal_Abort;
+	    end;	    
+	 end select;
+
+	 Manager.Finished;
 
             Prev := Num;
             delay 0.5;
