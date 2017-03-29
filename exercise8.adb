3281540f569b3990f6ab7225597777af1d48ac16
Space <avn@471.no>
2017-03-29 13:47:38 +0200

    More code (mostly indentation) cleanup

--- exercise8.adb
+++ exercise8.adb
@@ -3,7 +3,7 @@ use  Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
 
 procedure exercise8 is
 
-    Count_Failed    : exception;    -- Exception to be raised when counting fails
+    Count_Failed    : exception;
     Gen             : Generator;    -- Random number generator
 
     protected type Transaction_Manager (N : Positive) is
@@ -29,9 +29,9 @@ procedure exercise8 is
 	
         entry Wait_Until_Aborted when Aborted is
         begin
-		if Wait_Until_Aborted'Count = 0 then
-			Put_Line("All workers are aborted");
-		end if;
+			if Wait_Until_Aborted'Count = 0 then
+				Put_Line("All workers are aborted");
+			end if;
         end Wait_Until_Aborted;
 
         procedure Signal_Abort is
@@ -50,9 +50,6 @@ procedure exercise8 is
     Error_Rate : Constant := 0.15;  -- (between 0 and 1)
 	Result : Integer := 0;
     begin
-        -------------------------------------------
-        -- PART 1: Create the transaction work here
-        -------------------------------------------
 		if Random(Gen) < Error_Rate then
 			delay Duration(0.5 * Random(Gen));
 			raise Count_Failed;
@@ -73,29 +70,27 @@ procedure exercise8 is
         Put_Line ("Worker" & Integer'Image(Initial) & " started");
 
         loop
-	    
-	 select 
-	    Manager.Wait_Until_Aborted;
-	    Num := Prev + 5;
-	 then abort
-            Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
-            Round_Num := Round_Num + 1;
-	    begin
-	       Num := Unreliable_Slow_Add(Num);
-	    exception
-	       when Count_Failed =>
-		  Put_Line("Worker " & Integer'Image(Initial) & " aborted");
-		  Manager.Signal_Abort;
-	    end;	    
-	 end select;
-	 
-	 Put_Line ("Worker" & Integer'Image(Initial) & " value is" & Integer'Image(Num));
+			select 
+				Manager.Wait_Until_Aborted;
+				Num := Prev + 5;
+			then abort
+				Put_Line ("Worker" & Integer'Image(Initial) & " started round" & Integer'Image(Round_Num));
+				Round_Num := Round_Num + 1;
+				begin
+					Num := Unreliable_Slow_Add(Num);
+				exception
+					when Count_Failed =>
+					Put_Line("Worker " & Integer'Image(Initial) & " aborted");
+					Manager.Signal_Abort;
+				end;	    
+			end select;
 
-	 Manager.Finished;
+			Put_Line ("Worker" & Integer'Image(Initial) & " value is" & Integer'Image(Num));
 
-            Prev := Num;
-            delay 0.5;
+			Manager.Finished;
 
+			Prev := Num;
+			delay 0.5;
         end loop;
     end Transaction_Worker;
 
